968f4c9416bfa3ceceb67978a319f1383e9a74c6
Blazej Manczak <manczakb@gmail.com>
2018-12-01 23:30:43 +0100

    Add files via upload

--- codeFootball.py
+++ codeFootball.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 import os
+import time
+
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns  # also improves the look of plots
+sns.set()  # set Seaborn defaults
+plt.rcParams['figure.figsize'] = 10, 5  # default hor./vert. size of plots, in inches
+plt.rcParams['lines.markeredgewidth'] = 1  # to fix issue with seaborn box plots; needed after import seaborn
 
 os.getcwd()
 
@@ -24,7 +33,7 @@ def compare_to_part(x,y, pitch_devisions):
     #print("going_in",x,y)
     for i in range (len(pitch_devisions)):
         if ((pitch_devisions[i][0] <= x <= pitch_devisions[i][1]) & (pitch_devisions[i][2] <= y <= pitch_devisions[i][3])):
-            return i
+            return i+1
 
 ### example
 data = wholeDataset[wholeDataset['match_id'] == 30695]
@@ -38,11 +47,50 @@ compare_to_part(start_x,start_y, pitch_devisions)
 
 def create_div_cols(pitch_devisions, data):
     """ Adds columns saying what part of the pitch the pass originated from and to which part of the pitch the pass went"""
+    data1 = data.copy()
 
-    data['part_of_origin'] = data.apply(lambda row: compare_to_part(row['start_x'], row['start_y'], pitch_devisions), axis = 1)
-    data['part_of_dest'] = data.apply(lambda row: compare_to_part(row['end_x'], row['end_y'], pitch_devisions), axis = 1)
+    data1['part_of_origin'] = data1.apply(lambda row: compare_to_part(row['start_x'], row['start_y'], pitch_devisions), axis = 1)
+    data1['part_of_dest'] = data1.apply(lambda row: compare_to_part(row['end_x'], row['end_y'], pitch_devisions), axis = 1)
 
-    return data.copy()
+    return data1
+
+
 
 data_no_pitch_part = wholeDataset[wholeDataset['match_id'] == 30695]
 data = create_div_cols(pitch_devisions, data_no_pitch_part).copy()
+
+
+def create_pivot(data, start_time, end_time):
+    start = min(data['secs'])*start_time
+    end = max(data['secs'])*end_time
+    data = data[(start<data['secs']) & (data['secs']<end)].copy()
+    part_to_part_count = data.groupby(['part_of_origin', 'part_of_dest']).aggregate(['count'])['action_type']
+    part_to_part_count = part_to_part_count.reset_index()
+    pivot_table = part_to_part_count.pivot('part_of_origin', 'part_of_dest','count')
+    return pivot_table
+
+def create_heat(pivot_table):
+    ax = sns.heatmap(pivot_table, annot = True)
+    ax.set_xlabel("Destination of the ball")
+    ax.set_ylabel("Origin of the ball")
+    ax.set_title("Heatmap of passes", weight = "bold")
+
+pivot_table = create_pivot(data, 0, 100)
+create_heat(pivot_table)
+
+
+
+
+
+for i in range(1,10):
+    time.sleep(5)
+    sns.heatmap(pivot_table, annot = True)
+    plt.show()
+    fig.canvas.draw()
+    plt.clf()
+
+
+
+    #create_heat(create_pivot(data,10*(i-1), 10*i) )
+    #plt.clf()
+    #plt.show(block = False)
