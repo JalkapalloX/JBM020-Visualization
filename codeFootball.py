0763cbeb18fbc5bcd839a733a505b339284ba6c9
Blazej Manczak <manczakb@gmail.com>
2018-12-04 17:46:06 +0100

    Add files via upload

--- codeFootball.py
+++ codeFootball.py
@@ -4,9 +4,11 @@ import os
 import matplotlib
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.patches import Arc
+import time
 os.getcwd()
-
-wholeDataset = pd.read_csv('C:/Users/Hermii/Desktop/Vizualizations/all_passes.csv')
+os.chdir("/Users/blazejmanczak/Desktop/School/Year2/Q2/Visualization/Project_code/")
+wholeDataset = pd.read_csv('/Users/blazejmanczak/Desktop/School/Year2/Q2/Visualization/Project_code/all_passes.csv')
 wholeDataset[['start_x','end_x']] = wholeDataset[['start_x','end_x']]/100 * 68 # transofrming x coordinates to width in m
 wholeDataset[['start_y','end_y']] = wholeDataset[['start_y','end_y']]/100 * 105 # transofrming x coordinates to width in m
 wholeDataset.drop(['a','injurytime_play', 'through_ball', 'throw_ins'], axis = 1, inplace = True)
@@ -26,7 +28,7 @@ def compare_to_part(x,y, pitch_devisions):
     #print("going_in",x,y)
     for i in range (len(pitch_devisions)):
         if ((pitch_devisions[i][0] <= x <= pitch_devisions[i][1]) & (pitch_devisions[i][2] <= y <= pitch_devisions[i][3])):
-            return i
+            return i+1
 
 ### example
 pitch_devisions = make_division(68,105,3,4)
@@ -47,8 +49,78 @@ def create_div_cols(pitch_devisions, data):
 
 data_no_pitch_part = wholeDataset[wholeDataset['match_id'] == 30695]
 data = create_div_cols(pitch_devisions, data_no_pitch_part).copy()
-data.isnull().sum()
+#data.isnull().sum()
 
+### BLAZEJ'S PART BEGIN
+dd = create_div_cols(pitch_devisions, wholeDataset).copy()
+
+def create_pivot(data, start_time = -1, end_time = -1):
+    if start_time == -1 and end_time == -1:
+        start_time = min(data['secs'])
+        end_time = max(data['secs'])
+    start = min(data['secs'])*start_time
+    end = max(data['secs'])*end_time
+    data = data[(start<data['secs']) & (data['secs']<end)].copy()
+    part_to_part_count = data.groupby(['part_of_origin', 'part_of_dest']).aggregate(['count'])['action_type']
+    part_to_part_count = part_to_part_count.reset_index()
+    pivot_table = part_to_part_count.pivot('part_of_origin', 'part_of_dest','count')
+    return pivot_table
+
+def create_heat(pivot_table):
+    ax = sns.heatmap(pivot_table, annot = True)
+    ax.set_xlabel("Destination of the ball")
+    ax.set_ylabel("Origin of the ball")
+    ax.set_title("Heatmap of passes", weight = "bold")
+    #plt.show()
+    return ax
+
+
+pivot_table = create_pivot(data, 0, 100)
+plt.figure(figsize=(10,8))
+k = create_heat(pivot_table)
+k.get_figure().savefig("/Users/blazejmanczak/Desktop/testPhotos" + str(time.time())[:7]  + ".png")
+data.head()
+
+def generate_images(data, minutes, path):
+    if (minutes > max(data['mins'])) or (minutes < min(data['mins'])):
+        return "Invalid step. Please give step between {0} and {1}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    num_steps = max(data['mins'])//minutes
+    for i in range (1, num_steps+1):
+        if i != num_steps:
+            data_subset = data.loc[data['mins']<i*minutes].copy()
+        else: data_subset = data.copy()
+        table = create_pivot(data_subset)
+        plt.clf()
+        plot = create_heat(table)
+        plot.get_figure().savefig(path + str(i)  + ".png")
+
+generate_images(data, 30, "/Users/blazejmanczak/Desktop/testPhotos/")
+
+from scipy.misc import imread
+import pylab as pl
+
+dir = "/Users/blazejmanczak/Desktop/testPhotos/"
+files = sorted([dir + f for f in os.listdir(dir) if f[-3:] == 'png'])
+files
+
+img = None
+for f in files:
+    im=pl.imread(f)
+    if img is None:
+        img = pl.imshow(im)
+    else:
+        img.set_data(im)
+    pl.pause(.1)
+    pl.draw()
+
+
+
+### BLAZEJ'S PART END
+print("testing {0}".format(max(data['mins'])))
+
+### KACPER'S PART BEGIN
 dd = create_div_cols(pitch_devisions, wholeDataset).copy()
 
 #run this for making heatmap with 30 sec intervals on x axis and parts of field on y axis and color variable counts of passes
